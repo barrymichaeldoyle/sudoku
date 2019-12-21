a690b36ea7bdd0bf1a889587a8569024974832f5
Barry Doyle <barry.doyle@universal.co.za>
2019-12-21 20:29:42 +0200

    Updates

---
+++ precache-manifest.e32bb549f44b36e75a9c8c166df8ae02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7051c94240b7127f832c8c6f2e54244f",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "7cc1fdd4b3b1179c7eb8",
+    "url": "/sudoku/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1807b4c0cca75ed91f16",
+    "url": "/sudoku/static/js/2.f9aaff91.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sudoku/static/js/2.f9aaff91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cc1fdd4b3b1179c7eb8",
+    "url": "/sudoku/static/js/main.33869871.chunk.js"
+  },
+  {
+    "revision": "3e56837d8f07733c6c30",
+    "url": "/sudoku/static/js/runtime-main.d673c4f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sudoku/static/media/logo.5d5d9eef.svg"
+  }
+]);
