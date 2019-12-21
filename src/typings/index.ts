fd3f60ce9a9260c25312e1b86e47f2e64366325e
Barry Doyle <barry.doyle@universal.co.za>
2019-12-21 22:49:56 +0200

    updated typings

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,5 +1,7 @@
-export type n = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+export type NUMBERS = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
-export type Row = [n, n, n, n, n, n, n, n, n];
+export type N = 0 | NUMBERS;
+
+export type Row = [N, N, N, N, N, N, N, N, N];
 
 export type Grid = [Row, Row, Row, Row, Row, Row, Row, Row, Row];
