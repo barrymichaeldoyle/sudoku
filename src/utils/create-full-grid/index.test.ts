3e4b9ec782d42e3fb69694a317f4d3df6f5da92b
Barry Doyle <barry.doyle@universal.co.za>
2019-12-22 11:06:09 +0200

    implemented unit tests for create full grid

---
+++ src/utils/create-full-grid/index.test.ts
@@ -0,0 +1,13 @@
+import createFullGrid from "./";
+
+describe("createFullGrid", () => {
+  it("returns a 9X9 grid with value range 1 to 9", () => {
+    const grid = createFullGrid();
+    for (let row in grid) {
+      for (let col in grid[row]) {
+        expect(grid[row][col]).toBeGreaterThanOrEqual(1);
+        expect(grid[row][col]).toBeLessThanOrEqual(9);
+      }
+    }
+  });
+});
