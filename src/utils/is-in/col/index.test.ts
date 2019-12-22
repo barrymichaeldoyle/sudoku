b8e7972ef3a1f3c3c66dca7ed46c2df05d876724
Barry Doyle <barry.doyle@universal.co.za>
2019-12-22 21:27:47 +0200

    wrote unit tests for isInCol

---
+++ src/utils/is-in/col/index.test.ts
@@ -0,0 +1,39 @@
+import { GRID } from 'typings'
+
+import isInCol from './'
+
+describe('isInCol', () => {
+  it('returns true when value is in grid column', () => {
+    const grid: GRID = [
+      [8, 4, 2, 6, 5, 1, 3, 9, 7],
+      [5, 3, 7, 2, 8, 9, 6, 4, 1],
+      [6, 9, 1, 7, 3, 4, 5, 2, 8],
+      [1, 6, 3, 8, 4, 5, 9, 7, 2],
+      [7, 5, 8, 1, 9, 2, 4, 6, 3],
+      [9, 2, 4, 3, 7, 6, 1, 8, 5],
+      [4, 7, 6, 5, 1, 8, 2, 3, 9],
+      [2, 8, 5, 9, 6, 3, 7, 1, 4],
+      [3, 1, 9, 4, 2, 7, 8, 5, 6],
+    ]
+
+    expect(isInCol({ col: 0, grid, value: 9 })).toBeTruthy()
+    expect(isInCol({ col: 5, grid, value: 9 })).toBeTruthy()
+  })
+
+  it('returns false when value is not in grid column', () => {
+    const grid: GRID = [
+      [8, 4, 2, 6, 5, 1, 3, 9, 7],
+      [5, 3, 7, 2, 8, 0, 6, 4, 1],
+      [6, 9, 1, 7, 3, 4, 5, 2, 8],
+      [1, 6, 3, 8, 4, 5, 9, 7, 2],
+      [7, 5, 8, 1, 9, 2, 4, 6, 3],
+      [0, 2, 4, 3, 7, 6, 1, 8, 5],
+      [4, 7, 6, 5, 1, 8, 2, 3, 9],
+      [2, 8, 5, 9, 6, 3, 7, 1, 4],
+      [3, 1, 9, 4, 2, 7, 8, 5, 6],
+    ]
+
+    expect(isInCol({ col: 0, grid, value: 9 })).toBeFalsy()
+    expect(isInCol({ col: 5, grid, value: 9 })).toBeFalsy()
+  })
+})
