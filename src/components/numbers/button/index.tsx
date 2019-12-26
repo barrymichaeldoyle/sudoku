d53f1f3e493707da380781aac0c8ff6379695957
Barry Doyle <barry.doyle@universal.co.za>
2019-12-26 13:10:57 +0200

    buttons now work on grid

--- src/components/numbers/button/index.tsx
+++ src/components/numbers/button/index.tsx
@@ -1,6 +1,10 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { AnyAction, Dispatch } from 'redux'
 
-import { NUMBERS } from 'typings'
+import { IReducer } from 'modules'
+import { fillBlock } from 'modules/grid'
+import { BLOCK_COORDS, N, NUMBERS } from 'typings'
 
 import { Container } from './styles'
 
@@ -8,8 +12,29 @@ interface IProps {
   value: NUMBERS
 }
 
+interface IState {
+  selectedBlock?: BLOCK_COORDS
+  selectedValue: N
+}
+
 const Button: FC<IProps> = ({ value }) => {
-  return <Container>{value}</Container>
+  const state = useSelector<IReducer, IState>(
+    ({ grid: { selectedBlock, workingGrid } }) => ({
+      selectedBlock,
+      selectedValue:
+        workingGrid && selectedBlock
+          ? workingGrid[selectedBlock[0]][selectedBlock[1]]
+          : 0,
+    })
+  )
+  const dispatch = useDispatch<Dispatch<AnyAction>>()
+
+  const fill = useCallback(() => {
+    if (state.selectedBlock && state.selectedValue === 0)
+      dispatch(fillBlock(value, state.selectedBlock))
+  }, [dispatch, state.selectedBlock, state.selectedValue, value])
+
+  return <Container onClick={fill}>{value}</Container>
 }
 
 export default Button
