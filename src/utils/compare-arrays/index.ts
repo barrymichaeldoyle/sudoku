8d4630d5d0b316ec71b0ee6458212f4bb5977c03
Barry Doyle <barry.doyle@universal.co.za>
2020-01-05 12:51:03 +0200

    minor fix

--- src/utils/compare-arrays/index.ts
+++ src/utils/compare-arrays/index.ts
@@ -8,7 +8,7 @@ function compareArrays(arr1: any[], arr2: any[]): boolean {
 
   if (arr1.length !== arr2.length) return false
 
-  for (var i = 0, len = arr1.length; i < len; i++)
+  for (let i = 0, len = arr1.length; i < len; i++)
     if (!compareArrays(arr1[i], arr2[i])) return false
 
   return true
